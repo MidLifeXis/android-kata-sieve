8d1089bc87ed01ecbc569833a0295621fb56bf7a
Brian T. Wightman <MidLifeXis@wightmanfam.org>
2013-09-01 20:59:24 -0500

    use constant instead of magic numbers

--- src/com/mlxsolutions/sieve/Sieve.java
+++ src/com/mlxsolutions/sieve/Sieve.java
@@ -3,9 +3,10 @@ package com.mlxsolutions.sieve;
 public class Sieve {
 
     public int maxValue;
+    public final static int DEFAULT_MAX = 100;
 
     public Sieve () {
-	maxValue = 100;
+	maxValue = DEFAULT_MAX;
     }
 
     public Sieve (int newMax) {
--- tests/src/com/mlxsolutions/sieve/test/SieveTest.java
+++ tests/src/com/mlxsolutions/sieve/test/SieveTest.java
@@ -11,14 +11,14 @@ import com.mlxsolutions.sieve.Sieve;
 public class SieveTest extends AndroidTestCase {
 
     public void testConstructor__whenGivenMaxValue__setsMaxValueToProvidedValue() throws Throwable {
-        int expected = 250;
+        int expected = Sieve.DEFAULT_MAX * 2;
         Sieve sieve = new Sieve(expected);
         Assert.assertTrue(sieve.maxValue == expected);
     }
 
     public void testConstructor__whenNotGivenMaxValue__defaultsToDEFAULT_MAX() throws Throwable {
         Sieve sieve = new Sieve();
-        Assert.assertTrue(sieve.maxValue == 100);
+        Assert.assertTrue(sieve.maxValue == Sieve.DEFAULT_MAX);
     }
 
 }
